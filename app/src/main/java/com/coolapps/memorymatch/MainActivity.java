be18b6b3f917b25a385f49a08d6508bb0d5bf6fc
justin <justind417@gmail.com>
2019-04-13 11:27:36 -0500

    Main activity and 3x3  started.

--- app/src/main/java/com/coolapps/memorymatch/MainActivity.java
+++ app/src/main/java/com/coolapps/memorymatch/MainActivity.java
@@ -1,7 +1,10 @@
 package com.coolapps.memorymatch;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button play_3x3 = findViewById(R.id.Memory_btn);
+        play_3x3.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                play3x3();
+            }
+        });
+    }
+
+    public void play3x3(){
+        Intent intent = new Intent(getApplicationContext(),
+               Memory3x3.class );
+        startActivity(intent);
     }
 }
