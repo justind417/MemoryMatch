cb365b4aac6a1c60ca03077600b8e3f1032e6377
Larmstrong1626 <rarmstrong7@my.apsu.edu>
2019-04-15 19:41:41 -0500

    About activity

--- app/src/main/java/com/coolapps/memorymatch/AboutActivity.java
+++ app/src/main/java/com/coolapps/memorymatch/AboutActivity.java
@@ -12,45 +12,11 @@ public class AboutActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
-
-        Button play_3x3 = findViewById(R.id.Memory_btn);
-        play_3x3.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View v) {
-                play3x3();
-            }
-        });
-
-        Button about = findViewById(R.id.about_btn);
-        about.setOnClickListener(new View.OnClickListener(){
-            public void onClick(View v){
-                about();
-            }
-        });
-
-        Button play_4x4 = findViewById(R.id.mem_4x4btn);
-        play_4x4.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View v) {
-                play4x4();
-            }
-        });
-
     }
 
-    public void play3x3(){
-        Intent intent = new Intent(getApplicationContext(),
-               Memory3x3.class );
-        startActivity(intent);
-    }
-    public void play4x4(){
-        Intent intent = new Intent(getApplicationContext(),
-                Memory4x4.class );
-        startActivity(intent);
-    }
-    public void about()
-    {
-        Intent i = new Intent(getApplicationContext(),AboutActivity.class);
-        startActivity(i);
-    }
+        public void back(View view)
+        {
+            Intent i = new Intent(getApplicationContext(),MainActivity.class);
+            startActivity(i);
+        }
 }
